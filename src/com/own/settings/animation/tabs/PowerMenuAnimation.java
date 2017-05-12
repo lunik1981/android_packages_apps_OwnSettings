fbf3155c69d7294800c0dc61c7459c8f91508507
Alex <mazdarider23@gmail.com>
2017-05-12 17:34:40 +0200

    OwnSettings: Powermenu animations[2/2]
    Signed-off-by: OwnDroid <owndroid26@gmail.com>

--- src/com/own/settings/animation/tabs/PowerMenuAnimation.java
+++ src/com/own/settings/animation/tabs/PowerMenuAnimation.java
@@ -16,34 +16,73 @@
 
 package com.own.settings.animation.tabs;
 
-import android.os.Bundle;
-import android.support.v7.preference.ListPreference;
-import android.support.v7.preference.Preference;
-import android.support.v7.preference.Preference.OnPreferenceChangeListener;
-import android.support.v7.preference.PreferenceScreen;
-import android.support.v14.preference.PreferenceFragment;
-import android.support.v14.preference.SwitchPreference;
-import android.provider.Settings;
-
-import com.android.internal.logging.MetricsProto.MetricsEvent;
-
-import com.android.settings.R;
-import com.android.settings.SettingsPreferenceFragment;
+ import android.app.Activity;
+ import android.content.Context;
+ import android.content.ContentResolver;
+ import android.os.Bundle;
+ import android.os.SystemProperties;
+ import android.support.v7.preference.ListPreference;
+ import android.support.v14.preference.SwitchPreference;
+ import android.support.v7.preference.Preference;
+ import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+ import android.support.v7.preference.PreferenceScreen;
+ import android.view.LayoutInflater;
+ import android.view.View;
+ import android.view.ViewGroup;
+ import android.widget.Toast;
+ import android.provider.Settings;
+ 
+ 
+ import com.android.settings.R;
+ import com.android.settings.SettingsPreferenceFragment;
+ import com.android.settings.Utils;
+ 
+ import com.android.internal.util.own.AwesomeAnimationHelper;
+ 
+ import java.util.ArrayList;
+ import java.util.Arrays;
+ import java.util.HashSet;
+ import java.util.List;
+ 
+ import com.android.internal.logging.MetricsProto.MetricsEvent;
 
 public class PowerMenuAnimation extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
     private static final String TAG = "PowerMenuAnimation";
+    
+    private static final String POWER_MENU_ANIMATIONS = "power_menu_animations";
+    
+    private ListPreference mPowerMenuAnimations;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.power_menu_animation_tab);
+ 	    mContext = getActivity().getApplicationContext();
+ 		mContentRes = getActivity().getContentResolver();
+ 		
+        mPowerMenuAnimations = (ListPreference) findPreference(POWER_MENU_ANIMATIONS);
+        mPowerMenuAnimations.setValue(String.valueOf(Settings.System.getInt(
+                getContentResolver(), Settings.System.POWER_MENU_ANIMATIONS, 0)));
+        mPowerMenuAnimations.setSummary(mPowerMenuAnimations.getEntry());
+        mPowerMenuAnimations.setOnPreferenceChangeListener(this);
+
     }
 
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
-        return false;
+     @Override
+     public boolean onPreferenceChange(Preference preference, Object newValue) {
+ 		  boolean result = false;
+		if (preference == mPowerMenuAnimations) {
+            Settings.System.putInt(getContentResolver(), Settings.System.POWER_MENU_ANIMATIONS,
+                    Integer.valueOf((String) newValue));
+            mPowerMenuAnimations.setValue(String.valueOf(newValue));
+            mPowerMenuAnimations.setSummary(mPowerMenuAnimations.getEntry());
+            return true;
+		 }
+            preference.setSummary(getProperSummary(preference));
+  
+          return false;
     }
 
     @Override
