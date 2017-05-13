f6313a506e92d91d3494a2280a858585d4d78648
Alex <mazdarider23@gmail.com>
2017-05-13 11:29:12 +0200

    OwnSettings: Immersive Recents [2/2]
    Signed-off-by: OwnDroid <owndroid26@gmail.com>

--- src/com/own/settings/recents/tabs/GeneralRecents.java
+++ src/com/own/settings/recents/tabs/GeneralRecents.java
@@ -16,33 +16,65 @@
 
 package com.own.settings.recents.tabs;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContentResolver;
 import android.os.Bundle;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.app.Fragment;
+import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.ListPreference;
+import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
-import android.support.v7.preference.PreferenceScreen;
-import android.support.v14.preference.PreferenceFragment;
-import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
-
-import com.android.internal.logging.MetricsProto.MetricsEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
 
 public class GeneralRecents extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
     private static final String TAG = "GeneralRecents";
 
+	private static final String IMMERSIVE_RECENTS = "immersive_recents";
+	
+	private ListPreference mImmersiveRecents;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.general_recents_tab);
+        
+        final ContentResolver resolver = getContentResolver();
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mImmersiveRecents = (ListPreference) findPreference(IMMERSIVE_RECENTS);
+        mImmersiveRecents.setValue(String.valueOf(Settings.System.getInt(
+                getContentResolver(), Settings.System.IMMERSIVE_RECENTS, 0)));
+        mImmersiveRecents.setSummary(mImmersiveRecents.getEntry());
+		mImmersiveRecents.setOnPreferenceChangeListener(this);
+		
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mImmersiveRecents) {
+            Settings.System.putInt(getContentResolver(), Settings.System.IMMERSIVE_RECENTS,
+                    Integer.valueOf((String) newValue));
+            mImmersiveRecents.setValue(String.valueOf(newValue));
+            mImmersiveRecents.setSummary(mImmersiveRecents.getEntry());
+            return true;
+        }
         return false;
     }
 
