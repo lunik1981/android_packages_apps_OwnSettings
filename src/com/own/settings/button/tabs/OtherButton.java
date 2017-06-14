7bc3d821cec3ab302a861c0174a170a4333a9a46
Alex Cruz <mazdarider23@gmail.com>
2017-06-14 13:37:44 +0200

    OwnSettings: {SQUASHED} add entry for ScreenShot Delay and Type [2/2]
    Signed-off-by: OwnDroid <owndroid26@gmail.com>

--- src/com/own/settings/button/tabs/OtherButton.java
+++ src/com/own/settings/button/tabs/OtherButton.java
@@ -16,33 +16,77 @@
 
 package com.own.settings.button.tabs;
 
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.SystemProperties;
+import android.os.UserHandle;
 import android.support.v7.preference.ListPreference;
+import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v7.preference.PreferenceScreen;
-import android.support.v14.preference.PreferenceFragment;
-import android.support.v14.preference.SwitchPreference;
+import com.android.settings.accessibility.ToggleFontSizePreferenceFragment;
 import android.provider.Settings;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+import com.own.settings.javas.SeekBarPreference;
 
 public class OtherButton extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
     private static final String TAG = "OtherButton";
 
+    private static final String SCREENSHOT_TYPE = "screenshot_type";
+    private static final String SCREENSHOT_DELAY = "screenshot_delay";
+
+    private ListPreference mScreenshotType;
+    private SeekBarPreference mScreenshotDelay;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
+		ContentResolver resolver = getActivity().getContentResolver();
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.button_other_tab);
+        
+
+        mScreenshotType = (ListPreference) findPreference(SCREENSHOT_TYPE);
+        int mScreenshotTypeValue = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.SCREENSHOT_TYPE, 0);
+        mScreenshotType.setValue(String.valueOf(mScreenshotTypeValue));
+        mScreenshotType.setSummary(mScreenshotType.getEntry());
+        mScreenshotType.setOnPreferenceChangeListener(this);
+
+        mScreenshotDelay = (SeekBarPreference) findPreference(SCREENSHOT_DELAY);
+        int screenshotDelay = Settings.System.getInt(resolver,
+                Settings.System.SCREENSHOT_DELAY, 100);
+        mScreenshotDelay.setValue(screenshotDelay / 1);
+        mScreenshotDelay.setOnPreferenceChangeListener(this);
+
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if  (preference == mScreenshotType) {
+            int mScreenshotTypeValue = Integer.parseInt(((String) objValue).toString());
+            mScreenshotType.setSummary(
+                    mScreenshotType.getEntries()[mScreenshotTypeValue]);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.SCREENSHOT_TYPE, mScreenshotTypeValue);
+            mScreenshotType.setValue(String.valueOf(mScreenshotTypeValue));
+            return true;
+        } else if (preference == mScreenshotDelay) {
+            int screenshotDelay = (Integer) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.SCREENSHOT_DELAY, screenshotDelay * 1);
+            return true;
+         }
         return false;
     }
 
